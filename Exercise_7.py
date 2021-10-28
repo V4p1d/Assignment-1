79477d5d4ed476f2be9d4fa510416df6f363daf6
Pietro Ferraro <44121120+V4p1d@users.noreply.github.com>
2021-10-28 16:00:27 +0100

    Initial commit

---
+++ Exercise_7.py
@@ -0,0 +1,5 @@
+# Write a program that receives one input from the terminal (using input()).
+# The input needs to be an integer. The program will compute the factorial of 
+# this number and print it on the screen. If the number is negative the program will print 
+# "Factorial does not exist for negative numbers". Remember that the factorial of 0 is 1.
+# Example: input = 5, output = 5*4*3*2*1 = 120
---
+++ Exercise_8.py
@@ -0,0 +1,9 @@
+# Write a program that receives two inputs from the terminal (using input()).
+# Both inputs need to be integers. Assign them, respectively to a variable named "m"
+# and a variable named "n". The program will print on the terminal, the greatest common divisor (GCD)
+# of the two numbers.
+# Example: n = 10, m = 12, output = 2
+# Hint1: use a list to contain the divisors of n and one to contain the divisors of m. Use the method append() 
+# to add elements to the lists.
+# Hint2: there is a very smart and quick implementation of this that can be found online. I'd recommend to try and 
+# come up with your own version
